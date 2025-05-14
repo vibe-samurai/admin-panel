2d1a16d78fb726cbe88d04999b83e79a28742f67
KarinaFomina04 <karinafomina.19970804@gmail.com>
2025-05-14 15:40:04 +0200

    test data for fsd

---
+++ src/entities/types.ts
@@ -0,0 +1,7 @@
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+}
+
+//Файл: user/model/types.ts
---
+++ src/features/Auth/ui/LoginForm.tsx
@@ -0,0 +1,7 @@
+//test
+
+import React from 'react';
+
+export const LoginForm = () => {
+    return <form>Login Form</form>;
+};
