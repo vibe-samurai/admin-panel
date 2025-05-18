c79e284e21b77c4531cbe214bae03ef85049d7b1
Kozzzlova <kozlova.jully@gmail.com>
2025-05-19 01:06:02 +0300

    feat: add payment pagination slice to store

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { useDispatch, useSelector } from 'react-redux'
 
+import { paymentPaginationSlice } from '@/entities/PaymentsTable/model/slices/paymentsPaginationSlice'
 import { authSlice } from '@/features/auth/model/slices/authSlice'
 
 export const store = configureStore({
   reducer: {
     [authSlice.reducerPath]: authSlice.reducer,
+    [paymentPaginationSlice.reducerPath]: paymentPaginationSlice.reducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware(),
 })
